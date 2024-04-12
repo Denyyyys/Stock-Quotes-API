6b5055fd683c13730825f72e73fb10a467158ef1
Denyyyys <denys.fokashchuk@gmail.com>
2024-04-12 18:20:46 +0100

    Add assosiation to stock_quotes for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ApplicationRecord
   validates :ticker, presence: true, length: { minimum: 1, maximum: 5 }
   validates :name, presence: true, length: { minimum: 1 }
+  has_many :stock_quotes
 end
