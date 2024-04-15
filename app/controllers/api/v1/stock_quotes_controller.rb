4b288cc50fca117c49501dc951d97afcc5085d3e
Denyyyys <denys.fokashchuk@gmail.com>
2024-04-15 13:07:23 +0100

    Add empty controller with routes for stock quotes

---
+++ app/controllers/api/v1/stock_quotes_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class StockQuotesController < ApplicationController
+
+      def get_by_ticker # get all stock quotes of specific company
+      end
+
+      def destroy # delete one stock quote by id
+      end
+
+      def delete_all_by_ticker # delete all stock quotes of specific company
+      end
+
+      def create # create new stock quote
+      end
+
+      def update # update stock quote
+      end
+
+      def show # get one stock quote with id
+      end
+    end
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'stock_quotes/destroy'
+  get 'stock_quotes/create'
+  get 'stock_quotes/update'
+  get 'stock_quotes/show'
   get 'companies/index'
   get 'companies/create'
   get 'companies/destroy'
@@ -6,8 +10,17 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
+  namespace :api do
+    namespace :v1 do
+      resources :companies, only: [:index, :show, :create, :destroy], param: :ticker
 
+      resources :stock_quotes, only: [:destroy, :create, :update, :show] do
+        get 'ticker/:ticker', action: :get_by_ticker, on: :collection
+        delete 'ticker/:ticker', action: :delete_all_by_ticker, on: :collection
+      end
+    end
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
