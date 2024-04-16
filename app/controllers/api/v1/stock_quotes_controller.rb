b335948c0d6651bbd5ec2790d3eae97ea8c546a8
Denyyyys <denys.fokashchuk@gmail.com>
2024-04-16 21:25:41 +0100

    Update stock quotes controller to retrieve quotes for a specific company

--- app/controllers/api/v1/stock_quotes_controller.rb
+++ app/controllers/api/v1/stock_quotes_controller.rb
@@ -1,8 +1,14 @@
 module Api
   module V1
     class StockQuotesController < ApplicationController
-
       def get_by_ticker # get all stock quotes of specific company
+        company = Company.find_by("LOWER(ticker) = LOWER(?)", params[:ticker])
+        if company
+          stock_quotes = company.stock_quotes.sort_by { |quote| quote.updated_at }.reverse
+          render json: stock_quotes
+        else
+          render json: { error: "Company with ticker '#{params[:ticker]}' not found" }, status: :not_found
+        end
       end
 
       def destroy # delete one stock quote by id
