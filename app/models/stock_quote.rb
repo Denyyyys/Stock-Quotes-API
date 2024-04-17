c9e5eb2c28bda6c01a95c8e614a539f7cc9a8b1c
Denyyyys <denys.fokashchuk@gmail.com>
2024-04-17 17:27:51 +0100

    Add timestamp validator for created_at field

--- app/models/stock_quote.rb
+++ app/models/stock_quote.rb
@@ -1,4 +1,12 @@
 class StockQuote < ApplicationRecord
   belongs_to :company
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validate :valid_created_at_timestamp
+
+  private
+  def valid_created_at_timestamp
+    unless created_at.is_a?(Time) || created_at.is_a?(ActiveSupport::TimeWithZone)
+      errors.add(:created_at, "must be a valid timestamp")
+    end
+  end
 end
