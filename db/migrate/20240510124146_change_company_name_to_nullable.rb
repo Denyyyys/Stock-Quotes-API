c2f82c60c512e118db79c2cd3ad94d66cd6d7994
Denyyyys <denys.fokashchuk@gmail.com>
2024-05-14 17:38:06 +0100

    Fix rubocop offenses

--- db/migrate/20240510124146_change_company_name_to_nullable.rb
+++ db/migrate/20240510124146_change_company_name_to_nullable.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Change company name to be nullable
 class ChangeCompanyNameToNullable < ActiveRecord::Migration[7.1]
   def change
     change_column_null :companies, :name, true
